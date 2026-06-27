51e8f482c17a76a9fcf45414a031c2aecbee8075
Selvaganapathy R <selvas91290@gmail.com>
2026-06-27 09:40:26 +0530

    this file has a code for whileloop

---
+++ day6/hometask/prime_numbers.java
@@ -0,0 +1,39 @@
+package day6.hometask;
+import java.util.Scanner;
+
+
+public class prime_numbers {
+
+        public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter N: ");
+            int n = sc.nextInt();
+
+            int count = 0;
+            int num = 2;
+
+            while (count < n) {
+
+                boolean isPrime = true;
+
+                for (int i = 2; i <= num / 2; i++) {
+                    if (num % i == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime) {
+                    System.out.print(num + " ");
+                    count++;
+                }
+
+                num++;
+            }
+
+            sc.close();
+        }
+    }
+
