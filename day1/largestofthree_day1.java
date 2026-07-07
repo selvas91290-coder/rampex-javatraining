8c0b76489211d9e38cd8b6c51bc0d677e3b17b21
Selvaganapathy R <selvas91290@gmail.com>
2026-07-07 14:54:21 +0530

    this file has traffic program

---
+++ day1/largestofthree_day1.java
@@ -0,0 +1,24 @@
+package day1;
+
+public class largestofthree_day1 {
+        public static void main(String[] args) {
+            int a = 10;
+            int b = 20;
+            int c = 15;
+
+            if (a > b) {
+                if (b > c) {
+                    System.out.println("a is the largest");
+                } else {
+                    System.out.println("c is the largest");
+                }
+            } else {
+                if (b > c) {
+                    System.out.println("b is the largest");
+                } else {
+                    System.out.println("c is the largest");
+                }
+            }
+        }
+    }
+
