6540dbd65a62eb1a3afbe0c1502dcd4b7b0233a9
Selvaganapathy R <selvas91290@gmail.com>
2026-07-08 11:57:06 +0530

    this file has odd and even number
     programs

---
+++ day5/product.java
@@ -0,0 +1,16 @@
+package day5;
+import java.util.Scanner;
+
+public class product {
+        public static void main(String[] args){
+            int product = 1;
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter the number:");
+            int n = sc.nextInt();
+            for(int i = 1;i<=n;i++){
+                product = product*i;
+            }
+            System.out.println(product);
+        }
+    }
+
