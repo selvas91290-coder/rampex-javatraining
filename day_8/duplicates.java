bea2949a8c286a466edd28c7397d35b505155fe7
Selvaganapathy R <selvas91290@gmail.com>
2026-07-09 09:35:33 +0530

    this file has  7thday programs
     programs

---
+++ day_8/duplicates.java
@@ -0,0 +1,17 @@
+package day_8;
+
+public class duplicates {
+        public static void main(String[] args) {
+            int[] numbers = {2,5,7,2,8,5,9,7,7};
+            System.out.print("Duplicate elements: ");
+            for (int i = 0; i < numbers.length; i++) {
+                for (int j = i + 1; j < numbers.length; j++) {
+                    if (numbers[i] == numbers[j]) {
+                        System.out.print(numbers[i] + " ");
+                        break; // Prevents printing the same duplicate multiple times
+                    }
+                }
+            }
+        }
+    }
+
