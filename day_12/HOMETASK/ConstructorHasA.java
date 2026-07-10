a7c5e44ffd84c6434ad6e5572e299101d095a4cd
Selvaganapathy R <selvas91290@gmail.com>
2026-07-10 09:55:56 +0530

    this file has 12th day programs
     programs

---
+++ day_12/HOMETASK/ConstructorHasA.java
@@ -0,0 +1,28 @@
+package day_12.HOMETASK;
+
+
+
+    class Engine2 {
+
+        Engine2() {
+            System.out.println("Engine Created");
+        }
+    }
+
+    class Car6 {
+
+        Engine2 engine = new Engine2();
+
+        Car6() {
+            System.out.println("Car Created");
+        }
+    }
+
+    public class ConstructorHasA {
+
+        public static void main(String[] args) {
+
+            Car6 car = new Car6();
+        }
+    }
+
