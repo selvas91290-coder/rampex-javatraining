4d2a72495efb51e26ea2e59e02c2791cc17d3c7a
Selvaganapathy R <selvas91290@gmail.com>
2026-07-10 10:33:35 +0530

    this file has 14th day programs
     programs

---
+++ day_14/HOMETASK/InterfaceFlyable.java
@@ -0,0 +1,26 @@
+package day_14.HOMETASK;
+
+
+    interface Flyable {
+        void fly();
+    }
+    class Bird implements Flyable {
+        public void fly() {
+            System.out.println("Bird is flapping wings");
+        }
+    }
+    class Airplane implements Flyable {
+        public void fly() {
+            System.out.println("Airplane is engaging engines");
+        }
+    }
+    public class InterfaceFlyable {
+        public static void main(String[] args) {
+            Flyable[] flyers = {new Bird(), new Airplane()};
+
+            for (Flyable f : flyers) {
+                f.fly();
+            }
+        }
+    }
+
